13cb337cb6b0ac9c2f952a32ae9c64dcdeb1dd95
Mathias <mathias__vp@hotmail.com>
2018-01-10 13:05:12 -0500

    Add project files.

---
+++ FinalA.Tests/Controllers/TeamsControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// additional references
+using FinalA.Controllers;
+using FinalA.Models;
+using Moq;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FinalA.Tests.Controllers
+{
+    [TestClass]
+    public class TeamsControllerTest
+    {
+        TeamsController controller;
+        Mock<ITeamRepository> mock;
+        List<Team> teams;
+        
+        [TestInitialize] 
+        public void TestInitialize()
+        {
+            // arrange
+            mock = new Mock<ITeamRepository>();
+
+            teams = new List<Team> {
+                new Team { Name = "Toronto Maple Leafs", Points = 41 },
+                new Team { Name = "Vancouver Canucks", Points = 35 },
+                new Team { Name = "Montreal Canadiens", Points = 32 }
+            };
+
+            mock.Setup(m => m.Teams).Returns(teams.AsQueryable());
+
+            controller = new TeamsController(mock.Object);
+        }
+
+        [TestMethod] 
+        public void IndexValidLoadsTeams()
+        {
+            // act
+
+            // assert
+        }
+
+        [TestMethod]
+        public void DetailsValidId()
+        {
+            // act
+            var actual = (Team)controller.Details(1).Model;
+            // assert   
+            Assert.AreEqual(teams.ToList()[0], actual);
+        }
+
+        [TestMethod]
+        public void DetailsInvalidId()
+        { 
+            // act
+            var actual = (Team)controller.Details(99999999).Model;
+            // assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void DetailsMissingId()
+        {
+            // act
+            var actual = (Team)controller.Details(99999999).Model;
+            // assert
+            Assert.IsNull(actual);
+        }
+
+
+
+    }
+}
---
+++ FinalA/Controllers/TeamsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalA.Models;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace FinalA.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private ITeamRepository db;
+
+        public TeamsController()
+        {
+            this.db = new EFTeamRepository();
+        }
+
+        public TeamsController(ITeamRepository mockRepo)
+        {
+            this.db = mockRepo;
+        }
+
+        [Authorize]
+        // GET: Teams
+        public ViewResult Index()
+        {
+            var teams = db.Teams.Include(t => t.Division);
+            return View(teams.ToList());
+        }
+
+        // GET: Teams/Details/5
+        public ViewResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error");
+            }
+            Team team = db.Teams.SingleOrDefault(t => t.TeamID == id);
+
+            if (team == null)
+            {
+                return View("Error");
+            }
+            return View(team);
+        }
+    }
+}
